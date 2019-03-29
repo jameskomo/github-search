a429428af37cbe6792d3599d809ab177384b00e6
jameskomo <james.komoh@gmail.com>
2019-03-29 18:11:56 +0300

    Module imports done in root module (app.module.ts) and interpolation done in template file to display data fetched through GitHub API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -26,6 +29,7 @@ import { SearchService } from './search.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [SearchService],
