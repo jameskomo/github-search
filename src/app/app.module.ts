15eb019230bb5551b3ca96c7aba177b060fbe9a5
jameskomo <james.komoh@gmail.com>
2019-03-31 20:39:05 +0300

    Routing module added to root module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { GithubService } from './githubs/github.service';
 import { HttpClientModule } from '@angular/common/http';
+import { RoutingModule } from './routing/routing.module';
 
 import { AppComponent } from './app.component';
 import { GithubNavComponent } from './github-nav/github-nav.component';
@@ -19,7 +20,8 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RoutingModule,
   ],
   providers: [GithubService],
   bootstrap: [AppComponent]
